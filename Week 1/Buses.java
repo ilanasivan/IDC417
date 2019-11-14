7a1a12d68aaada5edfa6ed124d59a9e6cbaea746
Ilana <46295889+ilanasivan@users.noreply.github.com>
2019-11-15 00:18:05 +0200

    Create Buses.java

---
+++ Week 1/Buses.java
@@ -0,0 +1,37 @@
+
+public class Buses {
+/**The method checks what is the most cost-efficient way to transport tourists, with busses and/or shuttles, and provides a cost.
+ * The cost of a single bus is 500 ILS and the cost of a single shuttle is 300 ILS.
+ * A bus can take up to fifty passengers and a shuttle can take up to 12 passengers at a time.
+ * The method returns the total amount of busses and shuttles necessary to transport a given number of tourists and the total cost.
+ **/
+	public static void main(String[] args) {
+		//establish variables
+		int busCost = 500;
+		int shuttleCost = 300;
+		int maxBusPassengers= 50;
+		int maxShuttlePassengers = 12;
+		int numberOfShuttles=0;
+		int passengers = Integer.parseInt(args[0]);
+		int numberOfBuses = passengers/maxBusPassengers;
+		int passengersLeftOut = passengers%maxBusPassengers;
+
+		//validate
+		if (passengersLeftOut<0){
+			System.out.println("We can't send transportation for non-existant travelers");
+			return;
+		} else if (passengersLeftOut>12) {
+			numberOfBuses++;
+		} else if (passengersLeftOut<=12 && passengersLeftOut!=0){
+			numberOfShuttles++;
+		}
+
+		//establish price
+		int totalBusCost = numberOfBuses*busCost;
+		int totalShuttleCost = numberOfShuttles*shuttleCost;
+		int totalCost = totalBusCost+totalShuttleCost;
+
+		//generate output
+		System.out.println("Use " + numberOfBuses + " buses and " + numberOfShuttles + " shuttles at a cost of " + totalCost + " ILS");
+    }
+}
