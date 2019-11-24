dbd52730c0b6135b5b6663c23a278aafda4fcece
Ilana <46295889+ilanasivan@users.noreply.github.com>
2019-11-24 13:32:59 +0200

    Update PythagoreanIdentity.java

--- Week 1/PythagoreanIdentity.java
+++ Week 1/PythagoreanIdentity.java
@@ -1,32 +1,21 @@
-/*
-Assignment number : 1.4
-File Name : PythagoreanIdentity.java
-Name (First Last) : Ilana Sivan
-Student ID : 205634272
-Email : ilana.sivan@post.idc.ac.il
- */
 
 import java.lang.Math;
 
 public class PythagoreanIdentity {
-/** This method
- *
- *
- **/
 
     public static void main (String[] args) {
 
-        //store args
+        // Store args
         double input = Double.parseDouble(args[0]);
 
-        //creating formulas
+        // Creating formulas
         double inputSin = Math.sin(input);
         double inputCos = Math.cos(input);
 
-        //calculating the pythagorean identity and rounding it to the theoretical value
+        // Calculating the pythagorean identity and rounding it to the theoretical value
         double calculatedIdentity = Math.round(Math.pow(inputSin,2) + Math.pow(inputCos,2));
 
-        //generate output
+        // Generate output
         System.out.println("sin(" + input +")^2 + cos(" + input + ")^2 = " + calculatedIdentity);
 
     }
